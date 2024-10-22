8421ad5f4c47d7cbe3e9a8ca108cc2d38ea0150c
Lakshdeep Singh <lakshdeep.code@gmail.com>
2024-10-22 23:22:08 +0530

    Added custom hook to check admin role of user

---
+++ frontend/src/hooks/useUserRole.ts
@@ -0,0 +1,20 @@
+import useWorkspacesWithActive from "./useWorkspaceWithActive"
+import { useQueryClient } from "@tanstack/react-query"
+import { UserData } from "@/types"
+
+const useUserRole = () => {
+    const queryClient = useQueryClient()
+    const user = queryClient.getQueryData<UserData>(["user"])
+    const {currentWorkspace} = useWorkspacesWithActive()
+
+    let isAdmin = false;
+
+    if (user && currentWorkspace) {
+      if (currentWorkspace.admins.includes(user.userId)) {
+        isAdmin = true;
+      }
+    }
+  return isAdmin
+}
+
+export default useUserRole
