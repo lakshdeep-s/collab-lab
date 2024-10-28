198042a3e73ce842f339aeb064e5cbc0d086cefc
Lakshdeep Singh <lakshdeep.code@gmail.com>
2024-10-28 12:05:58 +0530

    Initial Setup for Resend Email Service

--- backend/src/controllers/invitationController.js
+++ backend/src/controllers/invitationController.js
@@ -1,4 +1,18 @@
 import { sendInvitationService } from "../services/invitation.service.js"
-export const sendInvitationController = async () => {
+import { NOT_FOUND } from "../constants/HTTPCodes.js"
+import appAssert from "../utils/appAssert.js"
 
-}
+export const sendInvitationController = async (req, res) => {
+    const userId = req.userId
+    appAssert(userId, 'User not found', NOT_FOUND)
+
+    const workspaceId = req.params.workspaceId
+    appAssert(workspaceId, 'Workspace not found', NOT_FOUND)
+
+    const {email} = req.body
+    appAssert(email, "Email not found", NOT_FOUND)
+
+    const result = await sendInvitationService(userId, workspaceId, email)
+
+    return res.status(200).json(result)
+}   
--- backend/src/services/invitation.service.js
+++ backend/src/services/invitation.service.js
@@ -1,3 +1,32 @@
-export const sendInvitationService = async () => {
-    
+import dotenv from "dotenv"
+dotenv.config()
+import { INTERNAL_SERVER_ERROR, NOT_FOUND, UNAUTHORIZED } from "../constants/HTTPCodes.js"
+import { WorkspaceModel } from "../model/workspace.model.js"
+
+import {Resend} from "resend"
+import appAssert from "../utils/appAssert.js"
+
+const resend_key = process.env.RESEND_API_KEY
+const resend = new Resend(resend_key)
+
+export const sendInvitationService = async (adminId, workspaceId, email) => {
+    const workspace = await WorkspaceModel.findById(workspaceId)
+    appAssert(workspace, "Workspace not found", NOT_FOUND)
+
+    const isAdmin = workspace.admins.includes(adminId)
+    appAssert(isAdmin, "User is not an admin", UNAUTHORIZED)
+
+    //Send invitation email with link here
+    const {data, error} = await resend.emails.send({
+        from: "lakshdeepsinghsiddhu@gmail.com",
+        to: [email],
+        subject: "Hello World",
+        html: "<strong>It Works</strong>"
+    })
+
+    if (error) {
+        appAssert(false, error.message, INTERNAL_SERVER_ERROR)
+    }
+
+    return data
 }
