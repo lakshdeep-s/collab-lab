b3798498c8267e69b02cc8d3241a0a6044a33786
Lakshdeep Singh <lakshdeep.code@gmail.com>
2024-11-06 15:35:58 +0530

    Fixed Workspace settings page

--- frontend/src/pages/WorkspaceSettingsPage.tsx
+++ frontend/src/pages/WorkspaceSettingsPage.tsx
@@ -2,17 +2,19 @@ import { WorkspaceSettingsContent } from "@/constants/WorkspaceContent"
 import useUserRole from "@/hooks/useUserRole"
 import EditWorkspaceForm from "@/components/workspace/EditWorkspaceForm"
 import TeamMemberList from "@/components/team/TeamMemberList"
-import useWorkspace from "@/hooks/useWorkspace"
+import useGetActiveWorkspace from "@/hooks/useGetActiveWorkspace"
+import useGetWorkspace from "@/hooks/useGetWorkspace"
 const c = WorkspaceSettingsContent
 
 const Settings = () => {
-  const {activeWorkspace} = useWorkspace()
+  const {currentWorkspaceId} = useGetActiveWorkspace()
+  const {workspace: currentWorkspace} = useGetWorkspace(currentWorkspaceId || '')
   const isAdmin = useUserRole()
 
   return (
     <div className="h-full overflow-scroll px-4 font-primary flex flex-col gap-8 no-scrollbar">
       <div className="flex flex-col gap-2">
-        <h1 className="text-2xl font-bold tracking-tight">Workspace Settings <span className="text-brand">{activeWorkspace?.name}</span></h1>
+        <h1 className="text-2xl font-bold tracking-tight">Workspace Settings <span className="text-brand">{currentWorkspace?.name}</span></h1>
         <p className="text-xsm text-muted-foreground">{c.header.subHeading}</p>
       </div>
 
@@ -21,18 +23,18 @@ const Settings = () => {
 
         {/* Admin */}
         {isAdmin && 
-          <EditWorkspaceForm workspace={activeWorkspace ? activeWorkspace : undefined}/>
+          <EditWorkspaceForm workspace={currentWorkspace ? currentWorkspace : undefined}/>
         }         
 
         {/* Non Admin */}
         {
           !isAdmin && 
-          <EditWorkspaceForm workspace={activeWorkspace ? activeWorkspace : undefined}/>
+          <EditWorkspaceForm workspace={currentWorkspace ? currentWorkspace : undefined}/>
         }
       </div>
 
       <div className="px-6 py-6 rounded-md flex flex-col text-xsm gap-4 bg-white shadow-md max-w-[800px]">
-        <TeamMemberList isAdminAccess={isAdmin} workspaceId={activeWorkspace?._id}/>
+        <TeamMemberList isAdminAccess={isAdmin} workspaceId={currentWorkspace?._id}/>
       </div>
     </div>
   )
